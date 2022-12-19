b2153460a6b628209bfc0f02214417377b97abf8
Moses Obotavo <iqtick18@gmail.com>
2022-12-19 12:56:14 -0800

    new repo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+nclude "main.h"
+/**
+ * mul - a function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: returns result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "min.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar (i + '0');
+		1++
+	}
+
+	_putchar ('\n');
+}
