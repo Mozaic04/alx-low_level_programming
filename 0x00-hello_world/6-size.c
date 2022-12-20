736826f100d05005c6b0faf918845d65b7e48b3f
Moses Obotavo <iqtick18@gmail.com>
2022-12-20 13:22:26 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of an int is %zu byte(s)\n", sizeof(int));
+	printf("size od a char is %zu byte(s)\n", sizeof(char));
+	printf("Size of a long int is %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is %zu byte(s)\n", sizeof(long long int));
+	print("Size of a float is %zu byte(s)\n", sizeof(float));
+	return (0);
+}
