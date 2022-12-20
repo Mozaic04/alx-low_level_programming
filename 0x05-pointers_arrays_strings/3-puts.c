0d530bdb65a954b3ac72039c80cfb840a1353893
Moses Obotavo <iqtick18@gmail.com>
2022-12-20 08:25:34 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - A  function that prints a string, followed by a new line
+ * @str:Input string
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = o; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
