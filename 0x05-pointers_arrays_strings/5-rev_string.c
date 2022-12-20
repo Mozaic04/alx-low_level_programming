2a7eeefc63bd34eec14e7b0e8f11172834a2d352
Moses Obotavo <iqtick18@gmail.com>
2022-12-20 09:18:44 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string  pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, k, temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		k++;
+		j--;
+	}
+}
