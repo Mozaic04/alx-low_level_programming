c6b2e846fa61ad070d97d5698c023855d61da171
Moses Obotavo <iqtick18@gmail.com>
2022-12-20 15:28:20 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,13 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(0)
+int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
 	putchar('\n');
 	return (0);
