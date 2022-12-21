2061943073c7e5ad2416a9ef47a17662fd73a664
Moses Obotavo <iqtick18@gmail.com>
2022-12-21 04:01:41 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * print_sign - function that prints the sign of a number..
@@ -16,7 +16,7 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar(45);
-		return (1);
+		return (-1);
 	}
 	else
 	{
